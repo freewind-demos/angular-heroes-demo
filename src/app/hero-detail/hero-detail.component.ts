a662eccf0bfce71ba45d3182b034da5fdc2d9f34
freewind <nowindlee@gmail.com>
2018-02-02 18:00:46 +0800

    In-memory data and CRUD

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {Hero} from '../hero';
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 
+import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
 
 @Component({
@@ -11,7 +11,6 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
   @Input() hero: Hero;
 
   constructor(private route: ActivatedRoute,
@@ -19,7 +18,7 @@ export class HeroDetailComponent implements OnInit {
               private location: Location) {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getHero();
   }
 
@@ -33,4 +32,8 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
